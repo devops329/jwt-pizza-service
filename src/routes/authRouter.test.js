23f2e2252a2217e81213eed8cfae0187fca29306
aaronzelenski <azelenski123@gmail.com>
2024-10-01 16:55:48 -0600

    Add(tests) for Auth where there are no Lint errors

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,38 +1,47 @@
 const request = require('supertest');
 const app = require('../service');
+const { DB } = require('../database/database.js');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
 
-beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
-});
+describe('authRouter testing...', () => {
+  let testUserId;
+  let testUserAuthToken;
 
-test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  beforeAll(async () => {
+    testUser.email = `${Math.random().toString(36).substring(2, 12)}@test.com`;
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    testUserId = registerRes.body.user.id;
+  });
 
-  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
-  expect(loginRes.body.user).toMatchObject(user);
-});
+  afterAll(async () => {
+    await DB.deleteUser(testUserId);
+  });
 
-test('Register a new user', async () => {
-    const newUser = {
-    name: 'New User',
-    email: 'newuser@example.com',
-    password: 'password123',
-  };
+  test('login', async () => {
+    const loginRes = await request(app).put('/api/auth').send(testUser);
+    expect(loginRes.status).toBe(200);
+    expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
-  const res = await request(app).post('/api/auth').send(newUser);
-  expect(res.status).toBe(200);
-  expect(res.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+    const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+    expect(loginRes.body.user).toMatchObject(user);
+    expect(password).toBe(testUser.password);
+  });
 
-  const { password, ...expectedUser } = newUser;
-  expect(res.body.user).toMatchObject({
-    ...expectedUser,
-    roles: [{ role: 'diner' }],
+  test('register', async () => {
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    expect(registerRes.status).toBe(200);
+    expect(registerRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+    const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+    expect(registerRes.body.user).toMatchObject(user);
+    expect(password).toBe(testUser.password);
+  });
+
+  test('logout', async () => {
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', 'Bearer ' + testUserAuthToken);
+    expect(logoutRes.status).toBe(200);
+    expect(logoutRes.body.message).toBe('logout successful');
   });
 });
