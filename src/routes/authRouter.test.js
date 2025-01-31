56e54567d5562aa315350e630063abdbe606fdc6
EvanSmith93 <evan.smith993@gmail.com>
2025-01-30 17:38:14 -0700

    Add register tests and fix issue found from tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,8 +4,14 @@ const app = require("../service");
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
 
+function getRandomEmail() {
+  return Math.random().toString(36).substring(2, 12) + "@test.com";
+}
+
+const tokenRegex = /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/;
+
 beforeEach(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  testUser.email = getRandomEmail();
   const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
 });
@@ -20,9 +26,7 @@ test("docs", async () => {
 test("login", async () => {
   const res = await request(app).put("/api/auth").send(testUser);
   expect(res.status).toBe(200);
-  expect(res.body.token).toMatch(
-    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
-  );
+  expect(res.body.token).toMatch(tokenRegex);
 
   const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
   expect(res.body.user).toMatchObject(user);
@@ -32,6 +36,29 @@ test("wrong password", async () => {
   const res = await request(app)
     .put("/api/auth")
     .send({ ...testUser, password: "ab" });
-  console.log(res.error);
   expect(res.status).toBe(404);
+});
+
+test("register", async () => {
+  const newUser = { ...testUser, email: getRandomEmail() };
+  const res = await request(app).post("/api/auth").send(newUser);
+  expect(res.status).toBe(200);
+  expect(res.body.token).toMatch(tokenRegex);
+
+  const { password, ...user } = { ...newUser, roles: [{ role: "diner" }] };
+  expect(res.body.user).toMatchObject(user);
+});
+
+test("duplicate register", async () => {
+  const newUser = { ...testUser, email: getRandomEmail() };
+  const res = await request(app).post("/api/auth").send(newUser);
+  expect(res.status).toBe(200);
+  const res2 = await request(app).post("/api/auth").send(newUser);
+  expect(res2.status).toBe(500);
+});
+
+test("no email register", async () => {
+  const newUser = { ...testUser, email: null };
+  const res = await request(app).post("/api/auth").send(newUser);
+  expect(res.status).toBe(400);
 });
