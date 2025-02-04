092001578764ef555024351d6f0218a5ef95e5e2
TSABM <tseegmillerabm@gmail.com>
2025-02-03 22:14:23 -0700

    auth Router logout works, must do update then auth Router next

--- tests/routes_tests/authRouter.test.js
+++ tests/routes_tests/authRouter.test.js
@@ -3,14 +3,14 @@ const app = require('../../src/service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
-
+//test register
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
   expectValidJwt(testUserAuthToken);
 });
-
+//test login
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
@@ -20,6 +20,19 @@ test('login', async () => {
   delete expectedUser.password;
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
+
+//test update user
+//test("update user", async ()=>{
+    //put request with path /api/auth/:userId
+//})
+//test logout
+
+test('login - logout', async () =>{
+    //const loginRes = await request(app).put('/api/auth').send(testUser);
+    const logoutRes = (await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`));
+    expect(logoutRes.status).toBe(200);
+    expect(logoutRes.body.message).toEqual("logout successful");
+})
 
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
