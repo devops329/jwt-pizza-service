69ffeeba08db14e4fc38c5ab8c91a110ed1caa37
TSABM <tseegmillerabm@gmail.com>
2025-02-04 12:27:58 -0700

    working on franchise tests

--- tests/routes_tests/franchiseRouter.test.js
+++ tests/routes_tests/franchiseRouter.test.js
@@ -1,3 +1,19 @@
 const request = require('supertest');
 const app = require('../../src/service');   
 
+beforeAll(async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const registerRes = await request(app)
+    .post('/api/franchise')
+    .set()
+    .send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    testUserID = registerRes.body.user.id;
+    expectValidJwt(testUserAuthToken);
+  });
+
+//test list all franchises
+test('list', async () => {
+    const listRes = await request(app).get('/api/franchises')
+    expect(listRes.status).toBe(200);
+  });
