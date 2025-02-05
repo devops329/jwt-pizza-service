64fc93d12405ea4d1bd1b079f5baf6334fb781ae
TSABM <tseegmillerabm@gmail.com>
2025-02-04 21:03:35 -0700

    franchise before all working

--- tests/routes_tests/franchiseRouter.test.js
+++ tests/routes_tests/franchiseRouter.test.js
@@ -1,20 +1,69 @@
 const request = require('supertest');
 const app = require('../../src/service');   
-/*
-beforeAll(async () => {
-    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    const registerRes = await request(app)
-    .post('/api/franchise')
-    .set()
-    .send(testUser);
-    testUserAuthToken = registerRes.body.token;
-    testUserID = registerRes.body.user.id;
-    expectValidJwt(testUserAuthToken);
-  });
-  */
+//const testFranchiseInfo = {name: "pizzaPocket", admins: [{"email": "f@jwt.com"}]};
+const { Role, DB } = require('../../src/database/database');
 
-//test list all franchises
-test('list', async () => {
-    const listRes = await request(app).get('/api/franchise')
-    expect(listRes.status).toBe(200);
-  });
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+    jest.setTimeout(60 * 1000 * 5); // 5 minutes
+  }
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+function expectValidJwt(potentialJwt) {
+    expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  }
+
+async function createAdminUser(userName, email, password) {
+    //loads an admin user into the database manually
+  let user = { password: password, roles: [{ role: Role.Admin }] };
+  user.name = userName
+  user.email = email
+
+  user = await DB.addUser(user);
+  return { ...user, password: password };
+}
+
+let adminUser;
+let testUserAuthToken;
+let franchise;
+describe("franchise tests", ()=>{
+    beforeAll(async () => {
+        let userName = randomName();
+        let email = userName + '@admin.com';
+        let password = 'toomanysecrets'
+        //create admin user
+        let admin = await createAdminUser(userName, email, password);
+        adminUser = {name : userName, email : email, password : password}
+        //login and grab the auth token
+        const loginRequest = await request(app).put('/api/auth').send(adminUser);
+        testUserAuthToken = loginRequest.body.token;
+        expect (loginRequest.status).toBe(200);
+        expectValidJwt(loginRequest.body.token);
+        
+        //create a franchise
+        franchise = {"name" : "testFranchise", "admins" : [{"email" : email}]}
+        const createRes = await request(app)
+        .post('/api/franchise')
+        .set('Authorization', `Bearer ${testUserAuthToken}`)
+        .send(franchise)
+        expect(createRes.status).toBe(200);
+      });
+    
+    //test list all franchises
+    test('list', async () => {
+        const listRes = await request(app).get('/api/franchise')
+        expect(listRes.status).toBe(200);
+      });
+    
+    //list a users franchises
+    
+    //create a new franchise
+    
+    //delete a franchise
+    
+    //create a new franchise store
+    
+    //delete a store
+    
+})
